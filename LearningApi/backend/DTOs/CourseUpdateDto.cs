d2d310c292a49302d540c23b0b01ede91f9d0b6c
Atharv Warkari <atharv.warkari@rediffmail.com>
2026-04-29 10:29:04 +0530

    Update DTOs

--- LearningApi/backend/DTOs/CourseUpdateDto.cs
+++ LearningApi/backend/DTOs/CourseUpdateDto.cs
@@ -9,11 +9,7 @@ public class CourseUpdateDto
     public string CourseName { get; set; } = "";
 
     [Required]
-<<<<<<< HEAD
     [Range(typeof(decimal), "1", "1000000")]
-=======
-    [Range(typeof(decimal), "0.01", "999999999.99", ErrorMessage = "Fee must be greater than 0")]
->>>>>>> 3b0d457 (Add DTOs)
     public decimal Fee { get; set; }
 
     [Required]
